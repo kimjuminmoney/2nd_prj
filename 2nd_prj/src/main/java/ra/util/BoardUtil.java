a0efe7380e1d8da0f0d4f117e2c07a3107ad301f
user <user@DESKTOP-COJ9858>
2023-11-03 16:04:02 +0900

    231103 홍지성 : 아오다영시치

--- 2nd_prj/src/main/java/ra/util/BoardUtil.java
+++ 2nd_prj/src/main/java/ra/util/BoardUtil.java
@@ -3,36 +3,68 @@ package ra.util;
 import ra.util.BoardUtilVO;
 
 public class BoardUtil {
-	static {
-		try {
-			Class.forName("jdbc/dbcp");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
+   private static BoardUtil bUtil;
+   
+   private BoardUtil() {
+      
+   }
+   
+   public static BoardUtil getInstance() {
+      if(bUtil == null) {
+         bUtil=new BoardUtil();
+      }//end if
+      return bUtil;
+   }//getInstance
 
-	public static DbConnection getConnection(String id, ) {
-		DbConnection con = null;
+   public String pageNation( BoardUtilVO buVO ) {
+      StringBuilder pageNation=new StringBuilder();
+      
+      int currentPage=buVO.getCurrentPage();//현재페이지
+      int totalPage=buVO.getTotalPage();//총 페이지 수
+      
+      
+      //1.화면에 보여줄 페이지 인덱스 수
+      int pageNumber=5;
+      //2.시작페이지 번호
+      int startPage=((currentPage-1)/pageNumber)*pageNumber+1;
+      //out.println("시작번호"+startPage);
+      //3.끝페이지 번호
+      int endPage=(((startPage-1)+pageNumber)/pageNumber)*pageNumber;
+      //out.println("끝번호"+endPage);
 
-		try {
-			con = DriverManager.getConnection(url, id, password);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return conn;
-	}
+      //구해진 끝페이지 번호가 총 페이지 보다 작다면, 총 페이지 수가 끝 번호가 된다.
+      if(totalPage <= endPage){
+         endPage = totalPage;
+      }//end if
 
-	public static void close(ResultSet rs, Statement stmt, Connection conn) {
-		try {
-			if (stmt != null) {
-				stmt.close();
-			}
-			if (conn != null) {
-				conn.close();
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
+      //6.첫 페이지가 인덱스 화면이 아닌경우
+      int movePage=0;
+      String prevLink="[ << ]";
+      if( currentPage > pageNumber ){
+         movePage=startPage-1;
+         pageNation.append("[ <a href='").append(buVO.getUrl()).append("?currentPage=").append(movePage).append("'> &lt;&lt;  </a>] ...");
+      }//end if
+
+      //7. 시작번호부터 끝번호까지 반복
+      movePage=startPage;
+      while( movePage <= endPage ){
+         if( movePage == currentPage ){ //현재 페이지와 이동할 페이지가 같다면 링크없이 인덱스 리스트 제공
+         pageNation.append("[ <span style='font-size:20px;'>").append(movePage).append("</span>]");
+         }else{
+            pageNation.append("[<a href='").append(buVO.getUrl()).append("?currentPage=").append(movePage).append("'>").append(movePage).append("</a>]");      
+         }
+         movePage++;
+      }//end while
+         
+      //8.뒤에 페이지가 더 있는 경우
+      if( totalPage > endPage){
+         movePage=endPage+1;
+         pageNation.append("... [ <a href='").append(buVO.getUrl()).append("?currentPage=").append(movePage).append("'> &gt;&gt;  </a>]");
+
+      }//end if
+      
+      return pageNation.toString();
+      
+   }//pageNation
 
 }
